f836e408af1f006bd0bc4da6607da2b7405c14a4
Rhys <xbyatomz@gmail.com>
2022-05-18 19:08:18 +0100

    Auto Scaling WIP

--- aws/autoscaling.tf
+++ aws/autoscaling.tf
@@ -4,14 +4,14 @@ resource "aws_placement_group" "exmaple_placement_group" {
 }
 
 resource "aws_autoscaling_group" "bar" {
-  name                      = "foobar3-terraform-test"
-  max_size                  = 5
-  min_size                  = 2
+  name                      = "example_terraform_asg"
+  max_size                  = 3
+  min_size                  = 1
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 4
+  desired_capacity          = 1
   force_delete              = true
-  placement_group           = aws_placement_group.test.id
+  placement_group           = aws_placement_group.exmaple_placement_group.id
   launch_configuration      = aws_launch_configuration.foobar.name
   vpc_zone_identifier       = [aws_subnet.example1.id, aws_subnet.example2.id]
 }
