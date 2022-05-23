778dc6605513b446c47942976098111572978b9f
Rhys Sharratt <xbyatomz@gmail.com>
2022-05-23 22:11:49 +0100

    Update autoscaling.tf

--- aws/ec2/autoscaling.tf
+++ aws/ec2/autoscaling.tf
@@ -13,11 +13,11 @@ resource "aws_autoscaling_group" "example_auto_scaling_group" {
   force_delete = true
 
   launch_template {
-    id = "${aws_launch_template.example_launch_template.id}"
-    version = "${aws_launch_template.example_launch_template.latest_version}"
+    id = aws_launch_template.example_launch_template.id
+    version = aws_launch_template.example_launch_template.latest_version
   }
 
-  vpc_zone_identifier = [ "${aws_subnet.private_subnet_1a.id}" ]
+  vpc_zone_identifier = [ aws_subnet.private_subnet_1a.id ]
 
   depends_on = [
     aws_launch_template.example_launch_template
